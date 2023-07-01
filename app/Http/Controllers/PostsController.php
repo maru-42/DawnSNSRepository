810989e767663796cc0aa37f508c1a92dfa65bf9
丸本将平 <>
2023-07-01 13:42:20 +0900

    4.2	ログインユーザーのつぶやきを表示 次でユーザー名とアイコンの表示とフォローユーザーの表示がくるので、いったんここまででセーブするためにコミット

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -13,15 +13,25 @@ class PostsController extends Controller
 {
     //
     public function index(){
-        return view('posts.index');
+        $postsList = DB::table('posts')
+            // ユーザーidの中で現在認証されているものを
+            ->where('user_id',Auth::id())
+            // 更新された順で
+            ->orderBy('updated_at','desc')
+            // 取得
+            ->get();
+        return view('posts.index',['posts'=>$postsList]);
     }
 
      public function create(Request $request)
     {
+        // index.blade.phpのリクエストから投稿内容を取得
         $post = $request->input('newPost');
+        // 現在認証されているユーザーのIDを取得
+        $userId = Auth::id();
+
         DB::table('posts')->insert([
-            // 現在認証されているユーザーのIDを取得
-            'user_id' =>Auth::id(),
+            'user_id' =>$userId,
             'posts' => $post,
             'created_at' => new DateTime(),
             'updated_at' => new DateTime(),
