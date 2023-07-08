7c9a809e3337884082d020e90aa75d3e0abdcc22
丸本将平 <>
2023-07-08 13:48:47 +0900

    6.1.2	フォローリスト/フォローユーザーのつぶやき一覧の設置 6.2.1	フォロワーリスト/フォロワーユーザーのアイコン一覧の設置 をやろうとしたが、途中でオフィス来社があり、別のところをやることになったので、いったんコメントアウトしてコミットプッシュした

--- resources/views/follows/followList.blade.php
+++ resources/views/follows/followList.blade.php
@@ -3,11 +3,26 @@
 @section('content')
 
 <table class='table table-hover'>
-  <!-- followscontrollerから送られてきたfollowsという名前で送られた、DBから取り出した画像s['follows'=>$followsList]を、foreachで取り出して、その一つ一つの呟きを$followという名前で使っていく -->
+  <!-- followscontrollerから送られてきたfollowsという名前で送られた、DBから取り出した画像s['follows'=>$followsList]を、foreachで取り出して、その一つ一つの画像を$followという名前で使っていく -->
   @foreach ($follows as $follow)
   <tr>
     <td><img src="{{ asset('/images/'.$follow->images)}}"></td>
   </tr>
   @endforeach
 </table>
+
+<!-- <table class='table table-hover'> -->
+<!-- followscontrollerから送られてきたfollowsという名前で送られた、DBから取り出したフォローユーザーのつぶやき一覧['follows'=>$followsList]を、foreachで取り出して、その一つ一つのフォローユーザーの呟きを$〇〇という名前で使っていく
+  ってことはfollowcontrollerで取得するものは画像だけではなく、呟きとユーザーネームもってこと？-->
+
+<!-- @foreach ($posts as $post)
+  <tr>
+    <td><img src="{{ asset('/images/'.$post->images)}}"></td>
+    <td>{{ $post->username}}</td>
+    <td>{{ $post->posts }}</td>
+    <td>{{ $post->created_at }}</td>
+  </tr>
+  @endforeach
+</table> -->
+
 @endsection
--- resources/views/follows/followerList.blade.php
+++ resources/views/follows/followerList.blade.php
@@ -1,4 +1,14 @@
 @extends('layouts.login')
 
 @section('content')
+
+
+<!-- <table class='table table-hover'>
+  followscontrollerから送られてきたfollowsという名前で送られた、DBから取り出した画像s['follows'=>$followsList]を、foreachで取り出して、その一つ一つの画像を$followという名前で使っていく
+  @foreach ($followers as $follower)
+  <tr>
+    <td><img src="{{ asset('/images/'.$follower->images)}}"></td>
+  </tr>
+  @endforeach
+</table> -->
 @endsection
