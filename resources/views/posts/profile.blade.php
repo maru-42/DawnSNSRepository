8e34b75a1deb9f360de175288ad13ebaca46206c
丸本将平 <>
2023-07-23 19:46:42 +0900

    8.2.1	プロフィール変更の入力フォームを設置 8.2.2	既存情報を入力フォームの初期値に設定 8.2.1はまだ完璧ではないが、、、

--- resources/views/posts/profile.blade.php
+++ resources/views/posts/profile.blade.php
@@ -3,15 +3,61 @@
 @section('content')
 
 <table class='table table-hover'>
-  <tr>
-    <td><a href="/profile"><img src="{{ asset('/images/'.$profile->images)}}"></a></td>
-    <td>{{ $profile->username}}</td>
-    <td>{{ $profile->mail}}</td>
-    <td>{{ $profile->password}}</td>
-    <td>{{ $profile->bio }}</td>
-    <td><a href="/profile"><img src="{{ asset('/images/'.$profile->images)}}"></a></td>
+  <div class="">
+    <a href="/profile"><img src="{{ asset('/images/'.$profile->images)}}"></a>
+  </div>
 
-  </tr>
+  {!! Form::open() !!}
+  {{ Form::token()}}
+
+  <div class="form-group row">
+    {{Form::label('inputName','UserName')}}
+    <div class="">
+      {{Form::text('inputName', $profile->username, ['class'=>'form-control', 'id'=>'inputName', 'placeholder'=>'UserName'])}}
+    </div>
+  </div>
+
+  <div class="form-group row">
+    {{Form::label('inputEmail','MailAdress')}}
+    <div class="">
+      {{Form::email('inputEmail', $profile->mail, ['class'=>'form-control', 'id'=>'inputEmail', 'placeholder'=>'MailAdress'])}}
+    </div>
+  </div>
+
+  <div class="form-group row">
+    {{Form::label('inputPassword','Password')}}
+    <div class="">
+      {{Form::password('inputPassword', ['class'=>'form-control', 'id'=>'inputPassword','readonly', 'placeholder'=>'●●●●●●'])}}
+    </div>
+  </div>
+
+  <div class="form-group row">
+    {{Form::label('inputPassword','new Password')}}
+    <div class="">
+      {{Form::password('inputPassword', ['class'=>'form-control', 'id'=>'inputPassword', 'placeholder'=>'new Password'])}}
+    </div>
+  </div>
+
+  <div class="form-group row">
+    {{Form::label('inputBio','Bio')}}
+    <div class="">
+      {{Form::textarea('inputBio', $profile->bio,['class'=>'form-control', 'id'=>'inputBio', 'placeholder'=>'Bio'])}}
+    </div>
+  </div>
+
+  <div class="form-group row">
+    {{Form::file('image', ['class'=>'custom-file-input', 'id'=>'fileimage'])}}
+    <div class="">
+      {{Form::label('image', 'ファイルを選択', ['class'=>'custom-file-label'])}}
+    </div>
+  </div>
+
+  <div class="form-group row">
+    {{Form::submit('更新', ['class'=>'btn btn-primary btn-block'])}}
+  </div>
+
+  {!! Form::close() !!}
+
 </table>
 
 
