14696b95faee002835298f6fc3a2a5aa37cc8ff8
丸本将平 <>
2023-07-23 13:41:30 +0900

    7.1.1	フォロー,フォロワーのユーザー名と自己紹介文の表示

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -5,7 +5,12 @@
 <table class='table table-hover'>
   <tr>
     <td><a href="/profile/{{ $profile->id}}"><img src="{{ asset('/images/'.$profile->images)}}"></a></td>
+    <th>Name</th>
     <td>{{ $profile->username}}</td>
+  </tr>
+  <tr>
+    <td></td>
+    <th>Bio</th>
     <td>{{ $profile->bio }}</td>
   </tr>
 </table>
